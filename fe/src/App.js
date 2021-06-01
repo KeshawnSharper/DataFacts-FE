1e6f76d64d5a24ccb93fd540c961045d61428ecd
KeshawnSharper <ksharper@studentmba.org>
2021-06-01 08:56:07 -0400

    Router

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from "./Login/Login"
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import {
+  BrowserRouter as Router,
+  Route,
+  NavLink,
+  Switch,
+  Link,
+  withRouter
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <Login /> 
+    <Router>
+    
+        
+    
+    <Switch>
+    <Route exact path="/" component={Login} />
+    <Route exact path="/register" component={Register} />
+   
+      {/* <ProtectedRoute exact path="/home" component={Content} /> */}
+    </Switch>
+    </Router>
+    
     </div>
   );
 }
