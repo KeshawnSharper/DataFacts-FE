a6edcfee75c15bf09f6ffa22c767582663aa2bf4
Keshawn Sharper <Ksharper@studentmba.org>
2021-06-02 16:52:06 -0400

    Commented actions and reducers

--- fe/src/actions/actions.js
+++ fe/src/actions/actions.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+// function to login users for register and login actions
 function loginUser(user, dispatch) {
   return axios
     .post(`https://datafacts-be.herokuapp.com/users/login`, user)
@@ -10,6 +11,7 @@ function loginUser(user, dispatch) {
     )
     .catch((err) => dispatch({ type: "LOGIN_FAIL", payload: err }));
 }
+// login action
 export function login(user) {
   return (dispatch) => {
     //   Load Login
@@ -18,19 +20,20 @@ export function login(user) {
     loginUser(user, dispatch);
   };
 }
+// register action that logs user in once registered
 export function register(user) {
   return (dispatch) => {
-    //   Load Login
+    //   Load register
     dispatch({ type: "REGISTER_LOADING" });
-    // send login credentials from Login page to the backend
+    // send register credentials to backend
     axios
       .post(`https://datafacts-be.herokuapp.com/users/register`, user)
       .then((response) => {
         dispatch({
-          // set the user object from redux state to the email and id from the response
-          type: "REGISTER",
-          payload: { username: response.data.username, id: response.data.id }
+          // Loggs that the user is successfully registered
+          type: "REGISTER"
         });
+        // Use the login user action to login the registered user
         loginUser(
           { username: user.username, password: user.password },
           dispatch
