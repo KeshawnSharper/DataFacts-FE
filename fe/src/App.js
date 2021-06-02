660f9ac9ffbaa00182ec4dfeb010937a0aad38a9
Keshawn Sharper <Ksharper@studentmba.org>
2021-06-02 12:13:51 -0400

    added router and package.json for codesandbox

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import Login from "./Login/Login"
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import Login from "./Login/Login";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-     <Login /> 
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
