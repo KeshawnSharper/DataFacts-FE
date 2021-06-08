561ecd73e518fb04a8e230b2561c84fde00d94df
KeshawnSharper <ksharper@studentmba.org>
2021-06-08 10:42:21 -0400

    Population tab design

--- fe/src/components/Tabs/Population/Population.js
+++ fe/src/components/Tabs/Population/Population.js
@@ -1,18 +1,74 @@
-import React from "react"
+import React, { useState } from "react"
 
 const Population = props => {
+   const [place,setPlace] = useState({city:"",state:""})
+    const handleChange = e => {
+        setPlace({
+            ...place,
+            [e.target.name]:e.target.value
+        })
+   }
     return (
-        <div>
+        <form>
+        <div className="con">
+          <header className="head-form">
+            <h2>Find Population</h2>
+            {/* If username or password is empty then send an error message, otherwise send a welcome message */}
+            {/* <p>
+              {submitted && error
+                ? "Invalid Credentials, Please Try again"
+                : submitted
+                ? "Please provide a username and/or password"
+                : "WELCOME, Login with your Datafacts credentials"}
+            </p> */}
+          </header>
+          <br />
+          <div className="field-set">
+            <span className="input-item">
+              <i className="fa fa-user-circle" />
+            </span>
+            {/* When the value changes the state will change based on property */}
             <input
-                className="form-input"
-                id="txt-input"
-                name="username"
-                type="text"
-                placeholder="Email"
-                required
-                onChange={(e) => handleChange(e)}
+              className="form-input"
+              id="txt-input"
+              name="State"
+              type="text"
+              placeholder="State"
+              required
+              onChange={(e) => handleChange(e)}
+            />
+            <br />
+            <span className="input-item">
+              <i className="fa fa-key" />
+            </span>
+            {/* When the value changes the state will change based on property */}
+
+            <input
+              className="form-input"
+              type="text"
+              name="City"
+              placeholder="City"
+              id="pwd"
+              onChange={(e) => handleChange(e)}
+            />
+            <span>
+              <i
+                className="fa fa-eye"
+                aria-hidden="true"
+                type="button"
+                id="eye"
               />
-            </div>
+            </span>
+            <br />
+            {/* Submits the login credentials if form is filled to be checked by the backend  */}
+            <button className="log-in">
+              {" "}
+              Find Population{" "}
+            </button>
+          </div>
+         
+        </div>
+      </form>
     )
 }
 export default Population
